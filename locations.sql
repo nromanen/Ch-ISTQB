889f92a77e91f6825fafed19df0692225910b499
Valeriy Bercha <valber.motorsport@gmail.com>
2019-05-20 20:11:27 +0300

    locations table with records

---
+++ locations.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO public.locations (id, city, country) VALUES (321, 'London', 'Great Britain');
+INSERT INTO public.locations (id, city, country) VALUES (322, 'Paris', 'France');
+INSERT INTO public.locations (id, city, country) VALUES (323, 'Berlin', 'Germany');
+INSERT INTO public.locations (id, city, country) VALUES (324, 'Marseille', 'France');
+INSERT INTO public.locations (id, city, country) VALUES (325, 'Strasbourg', 'France');
+INSERT INTO public.locations (id, city, country) VALUES (326, 'Bordeaux', 'France');
+INSERT INTO public.locations (id, city, country) VALUES (327, 'Barcelona', 'Spain');
+INSERT INTO public.locations (id, city, country) VALUES (328, 'Madrid', 'Spain');
+INSERT INTO public.locations (id, city, country) VALUES (329, 'Dortmund', 'Germany');
+INSERT INTO public.locations (id, city, country) VALUES (330, 'Antwerp', 'Belgium');
+
+
