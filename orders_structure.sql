954db6c74f43cf6c2713500fbcdc0d79df4cef4f
Valeriy Bercha <valber.motorsport@gmail.com>
2019-05-20 19:56:38 +0300

    final database structure

---
+++ orders_structure.sql
@@ -0,0 +1,399 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 10.7
+-- Dumped by pg_dump version 11.2
+
+-- Started on 2019-05-20 18:40:03
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 201 (class 1259 OID 41520)
+-- Name: customers; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.customers (
+    user_id integer NOT NULL
+);
+
+
+ALTER TABLE public.customers OWNER TO postgres;
+
+--
+-- TOC entry 196 (class 1259 OID 41505)
+-- Name: employees; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.employees (
+    user_id integer NOT NULL,
+    chief_id integer,
+    birth_date date,
+    hire_date date,
+    address character varying
+);
+
+
+ALTER TABLE public.employees OWNER TO postgres;
+
+--
+-- TOC entry 197 (class 1259 OID 41508)
+-- Name: locations; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.locations (
+    id integer NOT NULL,
+    city character varying,
+    country character varying
+);
+
+
+ALTER TABLE public.locations OWNER TO postgres;
+
+--
+-- TOC entry 202 (class 1259 OID 41523)
+-- Name: ordered_products; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.ordered_products (
+    orders_id integer,
+    products_id integer,
+    historical_price numeric,
+    quantity integer
+);
+
+
+ALTER TABLE public.ordered_products OWNER TO postgres;
+
+--
+-- TOC entry 199 (class 1259 OID 41514)
+-- Name: orders; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.orders (
+    id integer NOT NULL,
+    customer_id integer,
+    product_name_id integer,
+    order_date date,
+    responsible_employee integer,
+    delivery_location integer
+);
+
+
+ALTER TABLE public.orders OWNER TO postgres;
+
+--
+-- TOC entry 200 (class 1259 OID 41517)
+-- Name: products; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.products (
+    id integer NOT NULL,
+    product_name character varying,
+    product_category_id integer,
+    unit_price numeric,
+    location_id integer
+);
+
+
+ALTER TABLE public.products OWNER TO postgres;
+
+--
+-- TOC entry 203 (class 1259 OID 41526)
+-- Name: products_categories; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.products_categories (
+    id integer NOT NULL,
+    category_name character varying
+);
+
+
+ALTER TABLE public.products_categories OWNER TO postgres;
+
+--
+-- TOC entry 198 (class 1259 OID 41511)
+-- Name: users; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.users (
+    id integer NOT NULL,
+    first_name character varying,
+    last_name character varying,
+    location_id integer
+);
+
+
+ALTER TABLE public.users OWNER TO postgres;
+
+
+--
+-- TOC entry 2769 (class 2606 OID 41573)
+-- Name: customers customers_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.customers
+    ADD CONSTRAINT customers_pkey PRIMARY KEY (user_id);
+
+
+--
+-- TOC entry 2751 (class 2606 OID 41594)
+-- Name: employees employees_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT employees_pkey PRIMARY KEY (user_id);
+
+
+--
+-- TOC entry 2754 (class 2606 OID 41563)
+-- Name: locations locations_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.locations
+    ADD CONSTRAINT locations_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2763 (class 2606 OID 41551)
+-- Name: orders orders_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT orders_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2773 (class 2606 OID 41571)
+-- Name: products_categories products_categories_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products_categories
+    ADD CONSTRAINT products_categories_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2767 (class 2606 OID 41569)
+-- Name: products products_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT products_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2757 (class 2606 OID 41553)
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
+
+
+--
+-- TOC entry 2764 (class 1259 OID 41645)
+-- Name: fki_category; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_category ON public.products USING btree (product_category_id);
+
+
+--
+-- TOC entry 2752 (class 1259 OID 41605)
+-- Name: fki_chief; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_chief ON public.employees USING btree (chief_id);
+
+
+--
+-- TOC entry 2758 (class 1259 OID 41633)
+-- Name: fki_customer; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_customer ON public.orders USING btree (customer_id);
+
+
+--
+-- TOC entry 2770 (class 1259 OID 41561)
+-- Name: fki_customers; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_customers ON public.customers USING btree (user_id);
+
+
+--
+-- TOC entry 2759 (class 1259 OID 41714)
+-- Name: fki_delivery; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_delivery ON public.orders USING btree (delivery_location);
+
+
+--
+-- TOC entry 2760 (class 1259 OID 41708)
+-- Name: fki_employee; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_employee ON public.orders USING btree (responsible_employee);
+
+
+--
+-- TOC entry 2765 (class 1259 OID 41656)
+-- Name: fki_location_id; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_location_id ON public.products USING btree (location_id);
+
+
+--
+-- TOC entry 2761 (class 1259 OID 41639)
+-- Name: fki_product_name; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_product_name ON public.orders USING btree (product_name_id);
+
+
+--
+-- TOC entry 2771 (class 1259 OID 41617)
+-- Name: fki_products; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_products ON public.ordered_products USING btree (orders_id);
+
+
+--
+-- TOC entry 2755 (class 1259 OID 41670)
+-- Name: fki_users_location; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE INDEX fki_users_location ON public.users USING btree (location_id);
+
+
+--
+-- TOC entry 2781 (class 2606 OID 41640)
+-- Name: products category; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT category FOREIGN KEY (product_category_id) REFERENCES public.products_categories(id);
+
+
+--
+-- TOC entry 2775 (class 2606 OID 41600)
+-- Name: employees chief; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT chief FOREIGN KEY (chief_id) REFERENCES public.users(id);
+
+
+--
+-- TOC entry 2783 (class 2606 OID 41574)
+-- Name: customers customer; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.customers
+    ADD CONSTRAINT customer FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- TOC entry 2777 (class 2606 OID 41628)
+-- Name: orders customer; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT customer FOREIGN KEY (customer_id) REFERENCES public.customers(user_id);
+
+
+--
+-- TOC entry 2780 (class 2606 OID 41709)
+-- Name: orders delivery; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT delivery FOREIGN KEY (delivery_location) REFERENCES public.locations(id);
+
+
+--
+-- TOC entry 2779 (class 2606 OID 41703)
+-- Name: orders employee; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT employee FOREIGN KEY (responsible_employee) REFERENCES public.employees(user_id);
+
+
+--
+-- TOC entry 2782 (class 2606 OID 41651)
+-- Name: products location_id; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.products
+    ADD CONSTRAINT location_id FOREIGN KEY (location_id) REFERENCES public.locations(id);
+
+
+--
+-- TOC entry 2784 (class 2606 OID 41618)
+-- Name: ordered_products orders; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ordered_products
+    ADD CONSTRAINT orders FOREIGN KEY (orders_id) REFERENCES public.orders(id);
+
+
+--
+-- TOC entry 2778 (class 2606 OID 41634)
+-- Name: orders product_name; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.orders
+    ADD CONSTRAINT product_name FOREIGN KEY (product_name_id) REFERENCES public.products(id);
+
+
+--
+-- TOC entry 2785 (class 2606 OID 41623)
+-- Name: ordered_products products; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.ordered_products
+    ADD CONSTRAINT products FOREIGN KEY (products_id) REFERENCES public.products(id);
+
+
+--
+-- TOC entry 2774 (class 2606 OID 41595)
+-- Name: employees users; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.employees
+    ADD CONSTRAINT users FOREIGN KEY (user_id) REFERENCES public.users(id);
+
+
+--
+-- TOC entry 2776 (class 2606 OID 41665)
+-- Name: users users_location; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_location FOREIGN KEY (location_id) REFERENCES public.locations(id);
+
+
+-- Completed on 2019-05-20 18:40:04
+
+--
+-- PostgreSQL database dump complete
+--
+
