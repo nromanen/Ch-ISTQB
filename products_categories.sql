b60aaae2d1da610401fef0035347fb2b109904f1
Valeriy Bercha <valber.motorsport@gmail.com>
2019-05-20 20:12:59 +0300

    products_categories table with records

---
+++ products_categories.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO public.products_categories (id, category_name) VALUES (351, 'Fruits');
+INSERT INTO public.products_categories (id, category_name) VALUES (352, 'Vegetables');
+INSERT INTO public.products_categories (id, category_name) VALUES (353, 'Electronics');
+INSERT INTO public.products_categories (id, category_name) VALUES (354, 'Clothing');
+
